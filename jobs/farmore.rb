09e8c2af1e345b6605f85a26a3c9514e404c3ecd
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2014-08-12 19:09:16 -0500

    WIP: Start method for caching responses in Redis

--- jobs/farmore.rb
+++ jobs/farmore.rb
@@ -1,16 +1,56 @@
 require 'httparty'
 require 'date'
 require 'awesome_print'
+require 'redis'
+require 'json'
 
-auth = {
-  username: ENV[:WUFOO_USERNAME],
-  password: ENV[:WUFOO_PASSWORD],
+WUFOO_AUTH = {
+  username: ENV["WUFOO_USERNAME"],
+  password: ENV["WUFOO_PASSWORD"],
 }
+FORM_URL = "https://theaustinstone.wufoo.com/api/v3/forms/far-more-involve/entries.json"
+PAGE_SIZE = 100
+
+redis = Redis.new(url: ENV["REDISCLOUD_URL"])
+CACHE_PREFIX = "wufoo_response_page_"
 
 WEEKS = 6 # show signups for the last six weeks, rolling
 SECONDS_IN_A_WEEK = 60 * 60 * 24 * 7
 
-SCHEDULER.every '30m', :first_in => 0 do
+# get the JSON entries for the given page of a form. pages are always of size
+# PAGE_SIZE.
+def get_entries(page_number)
+  key = CACHE_PREFIX + page_number.to_s
+  cached_response = redis.get(key)
+
+  # return the cached response if it's already in redis
+  if cached_response != nil
+    return JSON.parse(cached_response)
+  end
+
+  # otherwise, fetch and cache it
+  resp = HTTParty.get(FORM_URL, query: {
+    pageSize: PAGE_SIZE,
+    pageStart: PAGE_SIZE * page_number,
+    basic_auth: WUFOO_AUTH,
+  })
+
+  # make sure we got a good response
+  throw "Wufoo response error: #{resp.code}" if resp.code != 200
+
+  # cache the response value
+  redis.set(key, JSON.generate(rest))
+
+  # if the page was not full, expire the key after a short time so we'll
+  # re-fetch the page in the future to check for new entries
+  if (resp["Entries"] || []).length < PAGE_SIZE
+    redis.expire(key, 60)
+  end
+
+  resp
+end
+
+SCHEDULER.every '1m', :first_in => 0 do
   STDERR.puts "======= Wufoo Fetch!"
   pages = 0
   page_start = 0
@@ -20,11 +60,7 @@ SCHEDULER.every '30m', :first_in => 0 do
     STDERR.puts "Fetching from EntryId #{page_start}."
     pages += 1
     resp = HTTParty.get("https://elbenshira.wufoo.com/api/v3/forms/stone-test/entries.json?pageSize=100&pageStart=#{page_start}",
-                        basic_auth: auth)
-
-    if resp.code == 421
-      STDERR.puts "======= Rate limit exceeded."
-    end
+                        basic_auth: WUFOO_AUTH)
 
     page_entries = resp["Entries"] || []
     if page_entries.length > 0
@@ -39,7 +75,6 @@ SCHEDULER.every '30m', :first_in => 0 do
   STDERR.puts "======= Fetched #{pages} pages, #{entries.count} entries."
 
   # Total signups over weeks
-
   this_week = Time.now.to_i / SECONDS_IN_A_WEEK
   first_week = this_week - WEEKS
   weekly_counts = [0] * (WEEKS + 1)
